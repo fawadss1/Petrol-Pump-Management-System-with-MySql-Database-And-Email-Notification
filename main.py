787eb067945738d1eeeac574d71215bf70bebb6c
Fawad Ali <71928305+fawadss1@users.noreply.github.com>
2021-01-18 20:02:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+try:
+    from DB_Connection import db
 
-from DB_Connection import db
-f = db.cursor()
-f.execute("SHOW TABLES")
-tbl = f.fetchall()
-if ("users",) in tbl:
-    import SignIn
-    import Welcome
-else:
-    import SignUp
-    import SignIn
-    import Welcome
+    f = db.cursor()
+    f.execute("SHOW TABLES")
+    tbl = f.fetchall()
+    if ("users",) in tbl:
+        import SignIn
+        import Welcome
+    else:
+        import SignUp
+        import SignIn
+        import Welcome
+except:
+    pass
