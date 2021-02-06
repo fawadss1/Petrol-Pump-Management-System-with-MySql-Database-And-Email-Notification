5d3a92fdaa348976b5a2abd54a8c05b4e45fb617
Fawad Ali <71928305+fawadss1@users.noreply.github.com>
2021-02-06 22:03:01 +0500

    Update SignIn.py

--- SignIn.py
+++ SignIn.py
@@ -1,6 +1,6 @@
 from Narrator import Narrator
 from DB_Connection import db
-
+import hashlib
 print("*" + "--" * 25 + "*")
 Narrator("Welcome To Fawad Petrol Pump Management System")
 print("*" + "~~" * 25 + "*")
@@ -10,6 +10,7 @@ while True:
     username = input().lower()
     Narrator(" Enter Your Password : ")
     password = input().lower()
+    password = hashlib.md5(password.encode('utf-8')).hexdigest()
     print("*" + "~~" * 20 + "*")
     f = db.cursor()
     f.execute("SELECT * FROM `users` WHERE username='" + username + "' AND password='" + password + "'")
